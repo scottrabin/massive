932a34c233bf153c6de7f1c2ce7fde33e1410f7f
Jeff Gaskill <mistergaskill@gmail.com>
2020-08-26 20:15:39 -0700

    Add hello world web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use hyper::{
+    service::{make_service_fn, service_fn},
+    Body, Request, Response, Server,
+};
+use std::{convert::Infallible, net::SocketAddr};
+
+async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    return Ok(Response::new("Hello, World".into()));
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let make_service =
+        make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(hello_world)) });
+
+    let server = Server::bind(&addr).serve(make_service);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e)
+    }
 }
